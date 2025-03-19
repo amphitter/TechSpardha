a7a3dd6bf166b8b7c925251826ad8ea9a696f769
Devansh Singh <145259402+amphitter@users.noreply.github.com>
2025-03-19 16:01:55 +0530

    Update Venue.tsx

--- components/Venue.tsx
+++ components/Venue.tsx
@@ -4,47 +4,22 @@ export default function Venue() {
   return (
     <section id="venue" className={styles.venueSection}>
       <h2 className={styles.subHeading}>Venue</h2>
-      <div
-        className={styles.venueContent}
-        style={{
-          color: 'white',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          gap: '23rem',
-          textAlign: 'center',
-        }}
-      >
+      <div className={styles.venueContent}>
         <div className={styles.venueDetails}>
           <h2>
-            <span style={{ color: 'orange', fontSize: '3rem' }}>C-1 Block</span>
+            <span>C-1 Block</span>
           </h2>
-          <address style={{ fontSize: '1.2rem', lineHeight: '1.6' }}>
+          <address>
             Global Institute Of Technology & Management,
             <br />
             Farukhnagar, Gurgaon
           </address>
         </div>
 
-        <div
-          className={styles.mapContainer}
-          style={{
-            width: '100%',
-            maxWidth: '400px',
-            height: '250px',
-            borderRadius: '15px',
-            overflow: 'hidden',
-            boxShadow: '0 10px 20px rgba(0,0,0,0.3)',
-          }}
-        >
+        <div className={styles.mapContainer}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38352.31708978086!2d76.88593105691123!3d28.31331503908143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d38e740000001%3A0xe4c205e5be809fee!2sGITM%20-%20Gurgaon%20Institute%20of%20Technology%20%26%20Management!5e0!3m2!1sen!2sin!4v1709135361664!5m2!1sen!2sin"
-            style={{
-              width: '100%',
-              height: '100%',
-              border: 'none',
-              borderRadius: '15px',
-            }}
+            className={styles.map}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             title="GITM Location Map"
