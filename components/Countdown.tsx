68f7995ad30e0756a2713a6cf794dec73e647908
Devansh Singh <145259402+amphitter@users.noreply.github.com>
2025-03-21 11:49:26 +0530

    Update Countdown.tsx

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -11,7 +11,7 @@ export default function Countdown() {
   });
 
   useEffect(() => {
-    const target = new Date('2025-03-28T00:00:00');
+    const target = new Date('2025-03-27T00:00:00');
     
     const interval = setInterval(() => {
       const now = new Date();
